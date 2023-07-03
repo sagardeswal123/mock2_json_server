b723704eefc2e9346212136c36169415cadecb0c
sagardeswal123 <sagardeswal798@gmail.com>
2023-07-03 11:45:57 +0530

    backend deployed

---
+++ index.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(4500,()=>{
+    console.log("JSON server is running")
+})
