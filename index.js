eefd8c494a0905d88eb9c3f3d8f72224af4c8555
sagardeswal123 <sagardeswal798@gmail.com>
2023-07-03 12:29:02 +0530

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const cors = require('cors');
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 server.listen(4500,()=>{
